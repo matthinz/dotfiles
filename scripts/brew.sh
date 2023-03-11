00518162348ca8d36dd0cee85bdc48737d0e8842
Matt Hinz <matthinz@gmail.com>
2023-03-11 13:43:33 -0800

    Dont' use brew bundle

--- scripts/brew.sh
+++ scripts/brew.sh
@@ -2,4 +2,13 @@ if [ ! which brew 2> /dev/null ]; then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
-brew bundle
+brew update
+
+brew install "asdf"
+brew install "deno"
+brew install "ffmpeg"
+brew install "git"
+brew install "git-lfs"
+brew install "go"
+brew install "pandoc"
+brew install "the_silver_searcher"
