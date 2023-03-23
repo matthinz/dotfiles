4ba2a30ddaefa42391e83974be52d0ff388701a3
Matt Hinz <matthinz@gmail.com>
2023-03-23 09:50:50 -0700

    Don't upgrade brew stuff on install

--- scripts/brew.sh
+++ scripts/brew.sh
@@ -2,12 +2,21 @@ if [ ! which brew 2> /dev/null ]; then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
-brew install "asdf"
-brew install "ffmpeg"
-brew install "git"
-brew install "git-lfs"
-brew install "go"
-brew install "pandoc"
-brew install "starship"
-brew install "the_silver_searcher"
-brew install "zsh-completions"
+function brew_install {
+  PACKAGE=$1; shift
+  if brew list $PACKAGE > /dev/null 2>&1; then
+    echo "$PACKAGE is already installed."
+  else
+    brew install $PACKGE
+  fi
+}
+
+brew_install asdf
+brew_install ffmpeg
+brew_install git
+brew_install git-lfs
+brew_install go
+brew_install pandoc
+brew_install starship
+brew_install the_silver_searcher
+brew_install zsh-completions
