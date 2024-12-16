0aeef5ad13e896c79ccaebab47c2d7c369d3afc1
Matt Hinz <matt.hinz@gsa.gov>
2024-12-16 09:51:43 -0800

    Fix crontab

--- files/bin/clean_up_desktop.sh.install.sh
+++ files/bin/clean_up_desktop.sh.install.sh
@@ -6,8 +6,7 @@ CLEAN_UP_DESKTOP_SCRIPT="$(cd $HOME && pwd)/bin/clean_up_desktop.sh"
 
 GREP_OUTPUT=$((crontab -l | grep "$CLEAN_UP_DESKTOP_SCRIPT") || true)
 
-
 if [ -z "$GREP_OUTPUT" ]; then
   echo "Add $CLEAN_UP_DESKTOP_SCRIPT to crontab"
-  echo "0 9 \* \* 1 $CLEAN_UP_DESKTOP_SCRIPT" | crontab -
+  echo "0 9 * * 1 $CLEAN_UP_DESKTOP_SCRIPT" | crontab -
 fi
